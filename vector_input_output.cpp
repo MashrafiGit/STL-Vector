dc362de344e4eb0bead36b2dd014919b1a74ac5a
Masrafi404 <mdmasrafi902@gmail.com>
2025-06-25 16:42:45 +0600

    vector input output

---
+++ vector_input_output.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    vector<int> v;
+
+    int x;
+    while (cin >> x)
+    {
+        v.push_back(x);
+    }
+
+    for (int i = 0; i < v.size(); i++)
+    {
+        cout << v[i] << " ";
+    }
+
+    return 0;
+}
